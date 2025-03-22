da0c0bb741da54025a22736ade9e0c65c20aae92
alfimonth <alvy1610@gmail.com>
2025-03-22 11:56:30 +0700

    Adding pages

---
+++ pages/about.vue
@@ -0,0 +1,30 @@
+<script>
+</script>
+
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto ea
+      illo fugit saepe obcaecati quos autem corrupti amet voluptates harum, est
+      maiores ducimus voluptas necessitatibus eius nobis tempora inventore odit.
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam, nobis.
+      Enim voluptatem aliquid necessitatibus itaque, molestias quod. Vero sit
+      nulla fugit laudantium voluptate impedit, earum error quisquam totam
+      explicabo quas!
+    </p>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
