2097e4f7e59471f464412980c060abb9267e1a87
alfimonth <alvy1610@gmail.com>
2025-03-22 14:41:32 +0700

    NuxtLink

--- pages/about.vue
+++ pages/about.vue
@@ -3,6 +3,17 @@
 
 <template>
   <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">Nuxt Dojo</NuxtLink>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    
     <h2>About</h2>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto ea
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">Nuxt Dojo</NuxtLink>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
     <p>Products</p>
-
   </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
